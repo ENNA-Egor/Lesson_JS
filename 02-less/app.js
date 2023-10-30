450fe10454d69cb427f02ae33204f0b98a5d9a66
ENNA-Egor <e.yudakov@gmail.com>
2023-10-30 23:24:02 +0300

    Решение с кодевар

--- 02-less/app.js
+++ 02-less/app.js
@@ -7,3 +7,18 @@ function mainCode(numbers) {
 }
 // console.log (doubledNumbers);
 mainCode()
+
+// function isLeapYear(year) {
+//     // TODO
+//         if (year % 400 === 0) {
+//           return true;
+//         } else if (year % 100 === 0) {
+//           return false;
+//         } else if (year % 4 === 0) {
+//           return true;
+//         } else {
+//           return false;
+//         }
+//       }
+
+//   console.log(isLeapYear(2100))

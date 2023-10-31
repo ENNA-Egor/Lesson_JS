4db6e5eeea6937d2b97918acf7bda86b2760f485
ENNA-Egor <e.yudakov@gmail.com>
2023-10-31 09:09:12 +0300

    html элементы

---
+++ htmlElem/app.js
@@ -0,0 +1,13 @@
+ const  title = document.querySelector('h1')
+
+const btn = document.querySelector('#btn');
+
+const subtitle = document.querySelector('section .gray');
+
+const sections = document.querySelectorAll('h2');
+
+
+console.log(title);
+console.log(btn);
+console.log(subtitle);
+console.log(sections);
